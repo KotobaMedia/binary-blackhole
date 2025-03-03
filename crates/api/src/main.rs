62d6e6880e05da1a1befee64a82019cc5bb22be5
Keitaroh Kobayashi <keita@kbys.me>
2025-03-04 07:18:13 +0900

    Add DB connection to health

--- crates/api/src/main.rs
+++ crates/api/src/main.rs
@@ -1,7 +1,8 @@
-use axum::Router;
+use crate::error::Result as AppResult;
 use axum::http::Method;
 use axum::response::Redirect;
 use axum::routing::get;
+use axum::{Router, extract::State};
 use lambda_http::{Error, run, tracing};
 use state::AppState;
 use tower_http::cors::{Any, CorsLayer};
@@ -14,8 +15,25 @@ mod threads;
 async fn root() -> Redirect {
     Redirect::temporary("https://www.bblackhole.com/")
 }
-async fn health() -> String {
-    "OK".to_string()
+async fn health(State(state): State<AppState>) -> AppResult<String> {
+    let mut chatter = state.chatter.lock().await;
+    let rows = chatter
+        .execute_query(
+            r#"
+            SELECT
+                'hello' as "name",
+                ST_Point(35, 135, 4326) as "geom"
+        "#,
+        )
+        .await?;
+    assert!(!rows.is_empty());
+    let row = &rows[0];
+    assert_eq!(
+        row.properties.get("name"),
+        Some(&serde_json::Value::String("hello".to_string()))
+    );
+    assert_eq!(row.geom, geo::Point::new(35.0, 135.0).into());
+    Ok("OK".to_string())
 }
 
 #[tokio::main(flavor = "current_thread")]
